fc82dc0f4e283b1319ad7452cbf7e033ce452f92
Emillyn Silva <emillyn_muniz@hotmail.com>
2022-10-02 14:58:51 -0300

    adicionando motores do react

--- aulas/01-fundamentos-reactjs/src/components/Comment.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,10 +1,25 @@
-import styles from './Comment.module.css';
-
+import { useState } from 'react';
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 
+import styles from './Comment.module.css';
 
-export function Comment() {
+
+export function Comment({content, onDeleteComment}) {
+  const [likeCount, setLikeCount] = useState(0);
+
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    //closures
+    setLikeCount((state) => {
+      return state + 1
+    });
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/diego3g.png" alt="" />
@@ -17,18 +32,18 @@ export function Comment() {
               <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={20} />
             </button>
           </header>
 
-          <p>Muito bom Devon, parabéns!! 👏👏</p>
+          <p>{content}</p>
         </div>
 
         <footer>
-          <button>
+          <button  onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
