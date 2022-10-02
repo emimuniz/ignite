f161b2f4d99ba331ce10915dda52800e95e51f06
Emillyn Silva <emillyn_muniz@hotmail.com>
2022-10-01 23:54:14 -0300

    Fundamentos-ReactJS

---
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,27 @@
+import { Header } from './components/Header'
+
+import styles from './App.module.css';
+
+import './global.css'
+import { SideBar } from './components/SideBar';
+import { Post } from './components/Post';
+
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      <div className={styles.wrapper}>
+        <SideBar/>
+        <main>
+        <Post/>
+        <Post/>
+        </main>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
